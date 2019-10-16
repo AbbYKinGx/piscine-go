2e2d9839b7a350900c228ff7c752d2ded0466e4b
AbbYKinGx <student@ALEM-a1d811.alem.school>
2019-10-16 23:36:53 +0600

    maingo3

--- printcomb.go
+++ printcomb.go
@@ -4,9 +4,9 @@ import "github.com/01-edu/z01"
 
 func PrintComb() {
 
-	for a := '0'; b <= '9'; a++ {
+	for a := '0'; a <= '9'; a++ {
 		for b := '1'; b <= '9'; b++ {
-			for c := '2'; b <= '9'; c++ {
+			for c := '2'; c <= '9'; c++ {
 				if a < b && b < c {
 					if a == '7' && b == '8' && c == '9' {
 						z01.PrintRune(a)
